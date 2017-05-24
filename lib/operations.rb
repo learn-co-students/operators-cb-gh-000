0fb42e075c7b35bac659d75773bf4cb78d1c2236
Edmundo Elizondo <edmundo.geg@hotmail.com>
2017-05-24 19:30:25 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+ #speed < 40 || speed > 60
+ if speed < 40 || speed > 60
+	 true
+ else
+	 false
+ end
 end
-
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
-	
-
-
