f1af96c8743841b8f4f7f4f9d569e384a934bc63
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-22 13:16:57 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+	if speed > 60
+		return true
+	elsif speed < 40
+		return true
+	else
+		return false
 end
 
 
 
 def not_safe?(speed)
-	
+	return (40 <= speed && speed <= 60) ? false : true
 end
-	
-
-
